82897d5d61db6f0cb5d16472798d2835abdff88a
jesle <jesleenaiqbal09@gmail.com>
2024-01-02 15:08:29 +0530

    committ

--- lib/gridview/gridview4.dart
+++ lib/gridview/gridview4.dart
@@ -1,23 +1,41 @@
 import 'package:flutter/material.dart';
-void main(){
-  runApp(MaterialApp(home: Gridview4(),));
-}
-class Gridview4 extends StatelessWidget {
+import 'package:flutter_october/listview/listview2.dart';
+import 'package:flutter_october/myWidget.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: Gridview4(),
+  ));
+}
+
+class Gridview4 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Gridview-Builder-fixedCrossAxixCount"),),
-      body: GridView.builder(itemCount: 20,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
-          itemBuilder: (context,index)=>
-              Container(
-    height: 50,
-    width: 50,
-    decoration: BoxDecoration(
-    image: DecorationImage(
-    image: AssetImage("assets/images/pic5.jpg"),
-    fit: BoxFit.fill)),)),
+      appBar: AppBar(
+        title: Text("Gridview-Builder-fixedCrossAxixCount"),
+      ),
+      body: GridView.builder(
+          itemCount: 20,
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+          itemBuilder: (context, index) => MyWidget(
+                label: Text("hello"),
+                onpressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Listview2()));
+                },
+                bgcolor: Colors.cyan,
+                image: Image.asset("assets/images/pic5.jpg"),
+              )
+          //           Container(
+          // height: 50,
+          // width: 50,
+          // decoration: BoxDecoration(
+          // image: DecorationImage(
+          // image: AssetImage("assets/images/pic5.jpg"),
+          // fit: BoxFit.fill)),)
+
+          ),
     );
   }
 }
---
+++ lib/myWidget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  final Color? bgcolor;
+  final Image? image;
+  final Widget? label;
+  VoidCallback? onpressed;
+
+  MyWidget(
+      {this.bgcolor, this.image, required this.label, required this.onpressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: bgcolor,
+      child: Column(children: [
+        Expanded(child: Container(child: image,)),
+        ElevatedButton(onPressed: onpressed, child: label)
+      ],),
+    );
+  }
+}

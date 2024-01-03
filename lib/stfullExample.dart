d30dc215d54be7885899072b90d785f14fd17d98
jesle <jesleenaiqbal09@gmail.com>
2024-01-03 22:20:55 +0530

    commit

--- lib/stfullExample.dart
+++ lib/stfullExample.dart
@@ -13,39 +13,77 @@ class STfullEx extends StatefulWidget {
 
 class _STfullExState extends State<STfullEx> {
   var bg = Colors.white;
+  bool showBulb = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: bg,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    bg = Colors.red;
-                  });
-                },
-                child: Text("red")),
-            ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    bg = Colors.green;
-                  });
-                },
-                child: Text("green")),
-            ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    bg = Colors.blue;
-                  });
-                },
-                child: Text("blue")),
-          ],
+        backgroundColor: bg,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      bg = Colors.red;
+                    });
+                  },
+                  child: Container(
+                      height: 40,
+                    width: 100,
+                    color: Colors.red,
+                    ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      bg = Colors.blue;
+                    });
+                  },
+                  child: Container(
+                    height: 40,
+                    width: 100,
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      bg = Colors.amber;
+                    });
+                  },
+                  child: Container(
+                    height: 40,
+                    width: 100,
+                    color: Colors.amber,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+            foregroundColor: Colors.yellow,
+            backgroundColor: Colors.red,
+            onPressed: () {
+              setState(() {
+                if (showBulb) {
+                  showBulb = false;
+                } else {
+                  showBulb = true;
+                }
+              });
+            },
+            child: Icon(
+                showBulb ? Icons.lightbulb : Icons.lightbulb_outline_rounded)));
   }
 }
